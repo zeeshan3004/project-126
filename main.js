6eea8fb35b422d4c0a4a0e60a2e146a6d331a0e0
zeeshan3004 <80521902+zeeshan3004@users.noreply.github.com>
2021-11-03 15:57:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+song1="";
+song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+function preload(){
+song1=loadSound("music.mp3");
+song2=loadSound("music2.mp3");
+}
+
+
+function draw(){
+image(video,0,0,600,500);
+}
+
+
+function setup(){
+canvas=createCanvas(600,500);
+canvas.position(1000,300);
+video=createCapture(VIDEO);
+video.hide();
+
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on("pose",gotPoses);
+}
+
+
+function play(){
+song.play();
+song.rate(1);
+song.setVolume(1);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX= results[0].pose.leftWrist.x;
+        leftWristY= results[0].pose.leftWrist.y;
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
+
+        rightWristX= results[0].pose.rightWrist.x;
+        rightWristY= results[0].pose.rightWrist.y;
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
+    }
+
+}
